450e9f8bd339cb71875a3e047f24189860c8fe82
Gleb Kazak <gkazak@estee.com>
2023-04-08 11:57:33 +0300

    Test4:dynamodb

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,10 +7,10 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://xfquf6mdke.execute-api.us-east-1.amazonaws.com/dev',
+    product: 'https://bdn87u3jd8.execute-api.us-east-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://xfquf6mdke.execute-api.us-east-1.amazonaws.com/dev',
+    bff: 'https://bdn87u3jd8.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
