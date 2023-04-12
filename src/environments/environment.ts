378ac7a35fb8e0e6dfdaf1d6a84f325dd28e9db7
Gleb Kazak <gkazak@estee.com>
2023-04-12 07:37:49 +0300

    Task-5: s3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://bdn87u3jd8.execute-api.us-east-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://zmolpoq8m1.execute-api.us-east-1.amazonaws.com/dev',
     bff: 'https://bdn87u3jd8.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
