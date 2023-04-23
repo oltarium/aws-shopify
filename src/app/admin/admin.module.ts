7f847445f6d9e962fe687e10884975ea03316692
Gleb Kazak <gkazak@estee.com>
2023-04-23 20:25:20 +0300

    Task-7: authorization

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,17 +9,19 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { OrdersComponent } from './orders/orders.component';
 import { MatTableModule } from '@angular/material/table';
 import { OrdersService } from './orders/orders.service';
-import { ManageProductsComponent } from './manage-products/manage-products.component';
+import { ManageProductsComponent, SnackComponent } from "./manage-products/manage-products.component";
 import { MatButtonModule } from '@angular/material/button';
 import { FilePickerModule } from '../shared/file-picker/file-picker.module';
 import { ManageProductsService } from './manage-products/manage-products.service';
 import { EditProductComponent } from './edit-product/edit-product.component';
+import { MatSnackBarModule, MatSnackBarRef } from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
     OrdersComponent,
     ManageProductsComponent,
     EditProductComponent,
+    SnackComponent
   ],
   imports: [
     AdminRoutingModule,
@@ -31,7 +33,11 @@ import { EditProductComponent } from './edit-product/edit-product.component';
     MatButtonModule,
     MatProgressSpinnerModule,
     ReactiveFormsModule,
+    MatSnackBarModule
   ],
-  providers: [OrdersService, ManageProductsService],
+  providers: [OrdersService, ManageProductsService, {
+    provide: MatSnackBarRef,
+    useValue: {}
+  },],
 })
 export class AdminModule {}
